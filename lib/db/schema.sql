43830b6cb3d5a91622dabf0f9407ce206f59b07c
Juma <gerald.juma@student.moringaschool.com>
2025-05-26 01:08:58 +0300

    Created lib/db/schema.sql defining authors, magazines, and articles tables

---
+++ lib/db/schema.sql
@@ -0,0 +1,22 @@
+-- Ya Kucreate authors table
+CREATE TABLE IF NOT EXISTS authors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Ya kucreate magazines table
+CREATE TABLE IF NOT EXISTS magazines (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL
+);
+
+-- Ya Kucreate articles table
+CREATE TABLE IF NOT EXISTS articles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(255) NOT NULL,
+    author_id INTEGER,
+    magazine_id INTEGER,
+    FOREIGN KEY (author_id) REFERENCES authors(id),
+    FOREIGN KEY (magazine_id) REFERENCES magazines(id)
+);
